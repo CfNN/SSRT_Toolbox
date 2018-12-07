782385b17a42e8ffe4983c7b1622b8967d4726cf
MorganBDT <morgan_talbot@alumni.brown.edu>
2018-12-07 11:04:03 -0500

    Add timestamps to variable list to be saved

--- Main_SSRT.m
+++ Main_SSRT.m
@@ -106,7 +106,7 @@ while (runningVals.currentTrial <= length(trials))
     [trials(runningVals.currentTrial).BlankOnsetTimestamp, trials(runningVals.currentTrial).BlankOffsetTimestamp] = ui.ShowBlank(settings.BlankDur, runningVals);
     
     % Autosave data in case the experiment is interrupted partway through
-    save(['subj' num2str(subjectNumber) '_sess' num2str(sessionNumber) '_' settings.ExperimentName '_AUTOSAVE.mat'], 'trials', 'settings', 'subjectNumber', 'sessionNumber', 'subjectHandedness');
+    save(['subj' num2str(subjectNumber) '_sess' num2str(sessionNumber) '_' settings.ExperimentName '_AUTOSAVE.mat'], 'trials', 'settings', 'subjectNumber', 'sessionNumber', 'subjectHandedness', 'triggerTimestamp', 'sessionStartDateTime', 'sessionStartFixationOnsetTimestamp', 'sessionStartFixationOffsetTimestamp');
     
     % Advance iterator to next trial
     runningVals.currentTrial = runningVals.currentTrial + 1;
@@ -122,7 +122,7 @@ PsychPortAudio('Close');
 clear ui filename;
 
 % Save the data to a .mat, delete autosaved version
-save(['subj' num2str(subjectNumber) '_sess' num2str(sessionNumber) '_' settings.ExperimentName '.mat'], 'trials', 'settings', 'subjectNumber', 'sessionNumber', 'subjectHandedness');
+save(['subj' num2str(subjectNumber) '_sess' num2str(sessionNumber) '_' settings.ExperimentName '.mat'], 'trials', 'settings', 'subjectNumber', 'sessionNumber', 'subjectHandedness', 'triggerTimestamp', 'sessionStartDateTime', 'sessionStartFixationOnsetTimestamp', 'sessionStartFixationOffsetTimestamp', 'sessionEndFixationOnsetTimestamp', 'sessionEndFixationOffsetTimestamp');
 delete(['subj' num2str(subjectNumber) '_sess' num2str(sessionNumber) '_' settings.ExperimentName '_AUTOSAVE.mat']);
 
 % Display SSD values from the end of the session (can be entered into
